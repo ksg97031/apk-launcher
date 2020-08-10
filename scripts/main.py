8ff377838368126e91d1b9a03b9f1c9eda941b13
nil.0 <nil.0@kakaocorp.com>
2020-08-10 19:56:15 +0900

    Fix version 1.0.0

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,20 @@
-import os
 import click
+import os
+import shutil
+import sys
 import time
-
-from adb import adb_commands
+from adb import adb_commands, usb_exceptions
 from androguard.core.bytecodes.apk import APK
+
+import builtins as __builtin__
+def print(*args, **kwargs):
+    sys.stdout.write('[apk-launcher] ')
+    return __builtin__.print(*args, **kwargs)
 
 @click.command()
 @click.argument('apk_path')
-def run(apk_path):
+@click.option('-n', '--nolog', required=False, default=False, is_flag=True)
+def run(apk_path, nolog):
     try:
         from adb import sign_m2crypto
 
@@ -41,23 +48,42 @@ def run(apk_path):
             print('%d: %s\tdevice' % (idx, device.serial_number))
         device = devices[int(input("Select device: "))-1]
 
-    adb.ConnectDevice(port_path=device.port_path, rsa_keys=[rsa_signer(path) for path in rsa_key_path])
+    try:
+        adb.ConnectDevice(port_path=device.port_path, rsa_keys=[rsa_signer(path) for path in rsa_key_path])
+    except usb_exceptions.ReadFailedError:
+        print(f"is your device offline? reconnect your device to pc.")
+        sys.exit(-1)
+    except Exception as e:
+        print(f"kill the current adb session(error msg: {str(e)})")
+        os.system("adb kill-server")
+        try:
+            adb.ConnectDevice(port_path=device.port_path, rsa_keys=[rsa_signer(path) for path in rsa_key_path]) # try again
+        except:
+            print(f"something wrong.. try reconnect your device to pc and run again.")
+            sys.exit(-1)
 
     apk = APK(apk_path)
-    print("Uninstall")
+    print(f"uninstall {apk.package} package")
     adb.Uninstall(apk.package)
 
-    print("Install")
+    print(f"reinstall {apk_path} file")
     adb.Install(apk_path, timeout_ms=1000000)
-    print("Start")
-
-    print(apk.package, apk.get_main_activity())
+    print(f"start {apk.package} on {apk.get_main_activity()}")
     adb.Shell("am start -n %s/%s" % (apk.package, apk.get_main_activity()))
-    pid = adb.Shell("ps -ef | grep %s | tr -s [:space:] ' ' | cut -d' ' -f2" % apk.package)
+    if not nolog:
+        adb.Logcat('clean')
+        time.sleep(1) # wait for activity
+        pid = adb.Shell("ps -ef | grep %s | tr -s [:space:] ' ' | cut -d' ' -f2" % apk.package)
+        print("ps -ef | grep %s | tr -s [:space:] ' ' | cut -d' ' -f2" % apk.package)
+        pid = pid.strip()
 
-    print("Logcat")
-    for i in adb.Logcat('| grep -F "%s"' % (pid), 1000000):
-        print(i)
+        print(f"\n\n[ Logcat - {apk.package}({pid}) ]\n") # recommand to use pidcat
+        for i in adb.Logcat('| grep -F "%s"' % (pid), 1000000):
+            __builtin__.print(i)
 
 if __name__ == '__main__':
+    if not shutil.which('adb'):
+        print('You need to install adb. (reference, https://stackoverflow.com/a/32314718)')
+        sys.exit(-1)
+    
     run()
